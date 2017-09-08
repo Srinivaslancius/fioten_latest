c8ed858e315be6723021557ff33860b021340bf3
srinivaslancius <srinivas@lanciussolutions.com>
2017-09-08 12:47:14 +0530

    changed add users page

--- manage_webmaster/add_users.php
+++ manage_webmaster/add_users.php
@@ -1,15 +1,12 @@
 <?php include_once 'admin_includes/main_header.php'; ?>
-<?php
-error_reporting(0);  
-if (!isset($_POST['submit']))  {
-  //If fail
-  echo "fail";
-} else  {
-  //If fail
-    //Save data into database
-    /*echo "<pre>";
-    print_r($_POST);die;*/
-    $user_name = $_POST['user_name'];
+<?php 
+  error_reporting(0);
+  if (!isset($_POST['submit']))  {
+    //If fail
+    echo "fail";
+  } else  { 
+      // //If success
+      $user_name = $_POST['user_name'];
     $user_email = $_POST['user_email'];
     $user_mobile = $_POST['user_mobile'];
     $user_country_id = $_POST['user_country_id'];
@@ -20,40 +17,38 @@ if (!isset($_POST['submit']))  {
     $user_address = $_POST['user_address'];
     $created_admin_id = $_SESSION['admin_user_id'];
     $created_at = date("Y-m-d h:i:s");
-    
-    $sql = "INSERT INTO users (`user_name`, `user_email`, `user_mobile`, `user_country_id`, `user_state_id`, `user_city_id`, `user_location_id`, `user_password`, `user_address`,`created_admin_id`, `created_at`, `status`) VALUES ('$user_name', '$user_email', '$user_mobile', '$user_country_id', '$user_state_id', '$user_city_id', '$user_location_id', '$user_password', '$user_address', '$created_admin_id', '$created_at', 1)";
+      $sql = "INSERT INTO users (`user_name`, `user_email`, `user_mobile`, `user_country_id`, `user_state_id`, `user_city_id`, `user_location_id`, `user_password`, `user_address`,`created_admin_id`, `created_at`, `status`) VALUES ('$user_name', '$user_email', '$user_mobile', '$user_country_id', '$user_state_id', '$user_city_id', '$user_location_id', '$user_password', '$user_address', '$created_admin_id', '$created_at', 1)";
     if($conn->query($sql) === TRUE){
        echo "<script type='text/javascript'>window.location='users.php?msg=success'</script>";
     } else {
        echo "<script type='text/javascript'>window.location='users.php?msg=fail'</script>";
     }
-}
+  }
 ?>
       <div class="site-content">
         <div class="panel panel-default">
           <div class="panel-heading">
             <h3 class="m-y-0">Users</h3>
           </div>
-          <div class="panel-body">            
+          <div class="panel-body">
             <div class="row">
               <div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <form data-toggle="validator" method="POST" autocomplete="off">
+                <form data-toggle="validator" method="POST">
                   <div class="form-group">
                     <label for="form-control-2" class="control-label">Name</label>
-                    <input type="text" name="user_name" class="form-control" id="form-control-2" placeholder="User Name" data-error="Please enter user name." required autofocus="on">
+                    <input type="text" name="user_name" class="form-control" id="form-control-2" placeholder="User Name" data-error="Please enter Name" required>
                     <div class="help-block with-errors"></div>
                   </div>
 
                   <div class="form-group">
                     <label for="form-control-2" class="control-label">Email</label>
-                    <input type="email" name="user_email" class="form-control" id="user_email" placeholder="Email" onkeyup="checkemail();" data-error="Please enter a valid email address." required>
-                    <span id="email_status" style="color: red;"></span>
+                    <input type="email" name="user_email" class="form-control" id="user_email" placeholder="Email" data-error="Please enter valid email address." required>
                     <div class="help-block with-errors"></div>
                   </div>
 
                   <div class="form-group">
                     <label for="form-control-2" class="control-label">Password</label>
-                    <input type="password" name="user_password" class="form-control" id="form-control-2" placeholder="Password" data-error="Please enter password." required>
+                    <input type="password" name="user_password" class="form-control" id="form-control-2" placeholder="Password" data-error="Please enter Password." required>
                     <div class="help-block with-errors"></div>
                   </div>
 
@@ -63,21 +58,22 @@ if (!isset($_POST['submit']))  {
                     <div class="help-block with-errors"></div>
                   </div>
 
-                  <?php $getCountries = getDataFromTables('lkp_countries',$status='0',$clause=NULL,$id=NULL,$activeStatus=NULL,$activeTop=NULL);  ?>
+                  <?php $getCountries = getDataFromTables('lkp_countries',$status='0',$clause=NULL,$id=NULL,$activeStatus=NULL,$activeTop=NULL);?>
                   <div class="form-group">
                     <label for="form-control-3" class="control-label">Select Country</label>
-                      <select name="user_country_id" id="user_country_id" class="custom-select" required onChange="getState(this.value);">
-                          <option value="">Select Country</option>
-                          <?php while($row = $getCountries->fetch_assoc()) {  ?>
-                              <option value="<?php echo $row['id']; ?>"><?php echo $row['country_name']; ?></option>
-                          <?php } ?>
-                      </select> 
+                    <select id="user_country_id" name="user_country_id" class="custom-select" data-error="This field is required." required onChange="getState(this.value);">
+                      <option value="">Select Country</option>
+                      <?php while($row = $getCountries->fetch_assoc()) { ?>
+                          <option value="<?php echo $row['id']; ?>"><?php echo $row['country_name']; ?></option>
+                      <?php } ?>
+                   </select>
+                    <div class="help-block with-errors"></div>
                   </div>
 
                   <div class="form-group">
                     <label for="form-control-3" class="control-label">Select State</label>
                     <select id="user_state_id" name="user_state_id" class="custom-select" data-error="This field is required." required onChange="getCities(this.value);">
-                      <option value="">Select State</option>
+                      <option value="">Select Country</option>
                    </select>
                     <div class="help-block with-errors"></div>
                   </div>
@@ -97,14 +93,14 @@ if (!isset($_POST['submit']))  {
                    </select>
                     <div class="help-block with-errors"></div>
                   </div>
-                  
+
                   <?php $getStatus = getDataFromTables('user_status',$status=NULL,$clause=NULL,$id=NULL,$activeStatus=NULL,$activeTop=NULL);?>
                   <div class="form-group">
                     <label for="form-control-3" class="control-label">Choose your status</label>
                     <select id="form-control-3" name="status" class="custom-select" data-error="This field is required." required>
                       <option value="">Select Status</option>
-                      <?php while($row = $getStatus->fetch_assoc()) {  ?>
-                        <option value="<?php echo $row['id']; ?>"><?php echo $row['status']; ?></option>
+                      <?php while($row = $getStatus->fetch_assoc()) { ?>
+                          <option value="<?php echo $row['id']; ?>"><?php echo $row['status']; ?></option>
                       <?php } ?>
                    </select>
                     <div class="help-block with-errors"></div>
@@ -123,6 +119,7 @@ if (!isset($_POST['submit']))  {
           </div>
         </div>
       </div>
+  
 <?php include_once 'admin_includes/footer.php'; ?>
 <script type="text/javascript">
 function getState(val) {
