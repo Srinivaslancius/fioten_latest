c68f93d2f794e11403c9240466e1ffe9cf154ba0
srinivaslancius <srinivas@lanciussolutions.com>
2017-10-17 10:37:05 +0530

    Changed config file

--- manage_webmaster/admin_includes/config.php
+++ manage_webmaster/admin_includes/config.php
@@ -2,42 +2,17 @@
 session_start(); 
 date_default_timezone_set("Asia/Kolkata");
 
-//Set session expiry time
-//Expire the session if user is inactive for 30
-//minutes or more.
-$expireAfter = 30; 
-//Check to see if our "last action" session
-//variable has been set.
-if(isset($_SESSION['last_action'])){
-    //Figure out how many seconds have passed
-    //since the user was last active.
-    $secondsInactive = time() - $_SESSION['last_action'];    
-    //Convert our minutes into seconds.
-    $expireAfterSeconds = $expireAfter * 60;    
-    //Check to see if they have been inactive for too long.
-    if($secondsInactive >= $expireAfterSeconds){
-        //User has been inactive for too long.
-        //Kill their session.
-        session_unset();
-        session_destroy();
-        header("Location: logout.php");
-  		exit;
-    }    
-}
-//End Session expiry time here
-
-
 $setcon = 2;
 if($setcon == 1) {
-	$servername = "localhost";
-	$username = "";
-	$password = "";
-	$dbname = "";
+    $servername = "localhost";
+    $username = "";
+    $password = "";
+    $dbname = "";
 } else {
-	$servername = "192.168.0.106";	
-	$username = "root";
-	$password = "root";
-	$dbname = "fioten_latest";
+    $servername = "192.168.0.100";  
+    $username = "root";
+    $password = "root";
+    $dbname = "fioten_latest";
 }  
 
 // Create connection
@@ -47,6 +22,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$base_url = "http://localhost/fioten_latest/";
+$base_url = "http://192.168.0.100/fioten_latest/";
  
 ?>
